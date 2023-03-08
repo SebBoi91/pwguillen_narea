641734c61f922f563cb1d476d1722e7cc43d80dd
Javier Guillén <sebboi98@gmail.com>
2023-03-08 17:38:50 -0500

    modificaciones
    modificaciones extra

--- products-api-company/build/index.js
+++ products-api-company/build/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var _express = _interopRequireDefault(require("express"));
+var _app = _interopRequireDefault(require("./app"));
+require("./database");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var app = (0, _express["default"])();
-app.listen(4000);
+_app["default"].listen(4000);
 console.log('Server listen on port', 4000);
