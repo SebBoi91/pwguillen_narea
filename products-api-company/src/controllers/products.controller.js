7b1542a454c9561b6adc83ebebaa9ea5789933d1
Javier Guillén <sebboi98@gmail.com>
2023-03-08 22:56:05 -0500

    User Schema
    Creado el user schema

--- products-api-company/src/controllers/products.controller.js
+++ products-api-company/src/controllers/products.controller.js
@@ -10,22 +10,31 @@ export const createProducts = async (req, res) => {
     const productSaved = await newProduct.save()
 
     res.status(201).json(productSaved)
-
-}
+};
 
 export const getProducts =  async(req,res) =>{
     const products = await Product.find();
     res.json(products)
-}
+};
 
-export const getProductById = (req, res) => {
+export const getProductById = async (req,res) =>{
+    const product = await Product.findById(req.params.productId);
+    res.status(200).json(product);
+};
 
-}
+export const updateProductById = async(req,res) =>{
+    const updatedProduct = await Product.findByIdAndUpdate(
+        req.params.productId,
+        req.body,
+        {
+          new:true,
+        }
+    );
+    res.status(200).json(updatedProduct)
+};
 
-export const updateProductById = (req, res) => {
-
-}
-
-export const deleteProductById = (req, res) => {
-
-}
+export const deleteProductById = async (req,res) =>{
+    const {productId} = req.params;
+    await Product.findByIdAndDelete(productId)
+    res.status(204).json();
+};
