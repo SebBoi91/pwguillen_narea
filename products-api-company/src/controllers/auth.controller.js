b69ccedec5fbcd27f26c087b926f0504f5b910b1
Javier Guillén <sebboi98@gmail.com>
2023-03-09 00:05:30 -0500

    Encriptado y autorizado
    Funciones para cifrar contraseña y controller de autorizacion

--- products-api-company/src/controllers/auth.controller.js
+++ products-api-company/src/controllers/auth.controller.js
@@ -1,27 +1,26 @@
-import { Schema, model } from "mongoose"
+import User from '../models/User'
+import jwt from 'jsonwebtoken'
+import config from "../config.js";
 
-const userSchema = new Schema({
-    username:{
-        type:String,
-        unique: true
-    },
-    email:{
-        type:String,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    roles: [{
-        ref: "Role",
-        type: Schema.Types.ObjectId
-    }]
- }, 
- {
-    timestamps:true,
-    versionKey: false,
- }
-);
+export const signUp = async (req, res) => {
+    
+    const {username, email, password, roles} = req.body;  
 
-export default userSchema;
+    const newUser = new User({
+        username,
+        email,
+        password: User.encryptPassword(password)
+    })
+
+    const savedUser = await newUser.save();
+
+    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+        expiresIn: 86400 // 24 horas
+    })
+
+    res.status(200).json({token})
+}
+
+export const signin = async (req, res) => {
+    res.json('singin')
+}
